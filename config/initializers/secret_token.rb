86377e967089f5c2ea1637f111eef05d0fa56841
VoraGunjan <Gunjan_Vora@intuit.com>
2013-10-24 14:04:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplieApp::Application.config.secret_token = 'a936112763e3acab1e3c121deff84ffa509b7217bdb95bd609d205b810632f8a289d1b58d96b7bc7881128c45a985e71f80820f4de326155d039ed20373950d8'
